841dab6dba8311ccefdbffc339112cac8c09242f
keiki <kkeki0410@gmail.com>
2023-07-18 16:37:29 +0900

    お問い合わせ一回目提出

---
+++ db/schema.rb
@@ -0,0 +1,14 @@
+
+
+ActiveRecord::Schema.define(version: 2023_07_18_052447) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "contacts", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.text "content"
+  end
+
+end
